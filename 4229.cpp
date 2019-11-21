22f05334fb55e674b6c725cc85cbf668b85c816e
Fairy-Yang <www.1163706928@qq.com>
2019-11-21 16:14:45 +0800

    第二次提交，修正了最后三个点的tle问题

--- 4229.cpp
+++ 4229.cpp
@@ -1,23 +1,24 @@
-//i dont know how to improve it
-//my code is almost the same as the code written by xiezhe which can be accepted;
-//time limit exceeded
+//when compling in vscode, ignore three warnings
 #include<iostream>
+//do not try to use cin and cout, scanf and printf have better speed
+//using c in_output, the last three tles are solved finally;
+#include<cstdio>
 #include<cstring>
 using namespace std;
 
 int main()
 {
   int n, max_index = 0, max_value=0;
-  cin >> n;
-  int data[60000]{0};
+  scanf("%d", &n);
+  int data[50005]{0};
   char operation[10];
   while(true)
   {
-    cin >> operation;
+    scanf("%s", operation);
     if(strcmp(operation, "add") == 0)
     {
       int i, j;
-      cin >> i >> j;
+      scanf("%d %d",&i,&j);
       ++data[j];
       if(data[j] == max_value && j < max_index)
         max_index = j;
@@ -30,7 +31,7 @@ int main()
     } 
     else if(strcmp(operation, "query")==0)
     {
-      cout << max_index << ' ' << max_value <<'\n';
+      printf("%d %d\n", max_index, max_value);
     }
     else
       break;
